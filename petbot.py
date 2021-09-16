2919dd8afd192ed07eb43e4fad558414d8f6a0e0
thelotswriter <thelotswriter@gmail.com>
2021-09-16 16:47:19 -0600

    Reworked base code. Added placeholder commands.

--- petbot.py
+++ petbot.py
@@ -1,6 +1,7 @@
 import os
 import random
 import discord
+from discord.ext import commands
 
 import numpy as np
 
@@ -11,32 +12,61 @@ import torch.nn.functional as F
 import torchvision
 
 token = os.getenv("DISCORD_TOKEN")
-my_guild = os.getenv("DISCORD_GUILD")
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='<@!886644051829460992> ', help_command=None)
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == my_guild:
-            break
+    print(f"{client.user} is connected!")
 
-    print(
-        f"{client.user} is connected to the following guild:\n"
-        f"{guild.name}(id: {guild.id})"
+@client.command()
+async def help(context):
+    await context.send(
+        'help - Show this list\n'
+        'begin - Get your new pet\n'
+        'show - Look at your pet\n'
+        'status - Check your pet\'s status\n'
+        'feed - Feed your pet\n'
+        'clean - Give your pet a bath\n'
+        'lights on/off - Turn the lights on or off\n'
+        'mute - Disable notifications\n'
+        'leave pet - Get rid of your current pet'
     )
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-#    await message.channel.send('Noticed')
-    if message.content.replace('!','').startswith(client.user.mention):
-        response = "Hi, I am " + client.user.mention + "! Nice to meet you!"
-        await message.channel.send(response)
-    else:
-        print(message.content)
-        print(client.user.mention)
+@client.command()
+async def begin(context):
+    await context.send('Sorry, we\'re all out of pets right now!')
+
+@client.command()
+async def show(context):
+    await context.send('Did you really expect to see something without a pet?')
+
+@client.command()
+async def status(context):
+    await context.send('Current status? There is no status.')
+
+@client.command()
+async def feed(context):
+    await context.send('Oops! All out of food!')
+
+@client.command()
+async def clean(context):
+    await context.send('Yeah, that\'s not happening')
+
+@client.command(aliases=['lights on'])
+async def lights_on(context):
+    await context.send('The lights are on but nobody\'s home!')
+
+@client.command(aliases=['lights off'])
+async def lights_off(context):
+    await context.send('Saving power? Saving the planet!')
+
+@client.command()
+async def mute(context):
+    await context.send('...')
+
+@client.command(aliases=['leave pet'])
+async def abandon(context):
+    await context.send('Really? That\'s so heartless. Also, you don\t have a pet now anyways, so no.')
 
 client.run(token)
